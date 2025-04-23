5b3cda46fdc83181380c1899c999b4343141a28a
KWaloen <k.waloen91@hotmail.com>
2025-04-23 12:52:34 +0200

    remove taglist

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -22,7 +22,6 @@ export const defaultContentPageLayout: PageLayout = {
     }),
     Component.ArticleTitle(),
     Component.ContentMeta(),
-    Component.TagList(),
     Component.DesktopOnly(Component.Graph()),
   ],
   left: [
